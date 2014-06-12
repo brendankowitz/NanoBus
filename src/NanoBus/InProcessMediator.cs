56afa86276ac77619edd3ae28eda85bc200ab64e
Brendan Kowitz <brendan.kowitz@gmail.com>
2014-06-12 16:10:18 +1000

    allow lifetime to be resolved

--- src/NanoBus/InProcessMediator.cs
+++ src/NanoBus/InProcessMediator.cs
@@ -1,54 +1,11 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Autofac;
-using Autofac.Features.OwnedInstances;
+﻿using Autofac;
 
 namespace NanoBus
 {
-    public class InProcessMediator : IBus
+    public class InProcessMediator : InProcessResolvedMediator
     {
-        private readonly ILifetimeScope _lifetimeScope;
-
-        public InProcessMediator(ILifetimeScope lifetimeScope)
+        public InProcessMediator(ILifetimeScope lifetimeScope) : base(() => lifetimeScope)
         {
-            _lifetimeScope = lifetimeScope;
-        }
-
-        public Task Send<TBusCommand>(TBusCommand busCommand) where TBusCommand : IBusCommand
-        {
-            using (var handler = _lifetimeScope.Resolve<Owned<IHandleCommand<TBusCommand>>>())
-            {
-                return handler.Value.Handle(busCommand);
-            }
-        }
-
-        public Task<TResponse> Request<TRequest, TResponse>(IBusRequest<TRequest, TResponse> busRequest)
-            where TRequest : IBusRequest<TRequest, TResponse> 
-            where TResponse : IBusResponse
-        {
-            using (var handler = _lifetimeScope.Resolve<Owned<IHandleRequest<TRequest, TResponse>>>())
-            {
-                return handler.Value.Handle((TRequest)busRequest);
-            }
-        }
-
-        public Task Publish<TBusEvent>(TBusEvent busEvent) where TBusEvent : IBusEvent
-        {
-            return Task.Factory.StartNew(() =>
-            {
-                using (var handlers = _lifetimeScope.Resolve<Owned<IEnumerable<IHandleMulticastEvent<TBusEvent>>>>())
-                {
-                    var tasks = handlers.Value
-                        .Select(h => h.Handle(busEvent))
-                        .ToArray();
-
-                    if (tasks.Any() == false)
-                        throw new BusException(string.Format("No event handlers are registered for '{0}'", typeof(TBusEvent).Name));
-
-                    Task.WaitAll(tasks);
-                }
-            });
         }
     }
 }
---
+++ src/NanoBus/InProcessResolvedMediator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Features.OwnedInstances;
+
+namespace NanoBus
+{
+    public class InProcessResolvedMediator : IBus
+    {
+        private readonly Func<ILifetimeScope> _lifetimeScope;
+
+        public InProcessResolvedMediator(Func<ILifetimeScope> lifetimeScope)
+        {
+            _lifetimeScope = lifetimeScope;
+        }
+
+        public Task Send<TBusCommand>(TBusCommand busCommand) where TBusCommand : IBusCommand
+        {
+            using (var handler = _lifetimeScope().Resolve<Owned<IHandleCommand<TBusCommand>>>())
+            {
+                return handler.Value.Handle(busCommand);
+            }
+        }
+
+        public Task<TResponse> Request<TRequest, TResponse>(IBusRequest<TRequest, TResponse> busRequest)
+            where TRequest : IBusRequest<TRequest, TResponse>
+            where TResponse : IBusResponse
+        {
+            using (var handler = _lifetimeScope().Resolve<Owned<IHandleRequest<TRequest, TResponse>>>())
+            {
+                return handler.Value.Handle((TRequest)busRequest);
+            }
+        }
+
+        public Task Publish<TBusEvent>(TBusEvent busEvent) where TBusEvent : IBusEvent
+        {
+            using (var handlers = _lifetimeScope().Resolve<Owned<IEnumerable<IHandleMulticastEvent<TBusEvent>>>>())
+            {
+                var tasks = handlers.Value
+                    .Select(h => h.Handle(busEvent))
+                    .ToArray();
+
+                if (tasks.Any() == false)
+                    throw new BusException(string.Format("No event handlers are registered for '{0}'", typeof(TBusEvent).Name));
+
+                Task.WaitAll(tasks);
+                return Task.Factory.StartNew(() => true);
+            }
+        }
+    }
+}
